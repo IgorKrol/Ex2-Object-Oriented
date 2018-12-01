c8baea896ac690b36f90963ea85a98d62e55daf0
IgorKrol <igork08@gmail.com>
2018-12-01 18:48:02 +0200

    project

---
+++ Algo/MultiCSV.java
@@ -0,0 +1,18 @@
+package Algo;
+
+import GIS.GIS_project;
+import GIS.Project;
+
+public class MultiCSV {
+
+	GIS_project p;
+	
+	public MultiCSV(String path) {
+		p = new Project(description);
+	}
+	public Project MultiCSV(String path) {
+		
+		
+		return null;
+	}
+}
