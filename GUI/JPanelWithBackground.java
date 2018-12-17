77d4e91b435b8f97fb27b7eb19cf93d9cde24854
IgorKrol <igork08@gmail.com>
2018-12-17 15:33:33 +0200

    More gui problems

--- GUI/JPanelWithBackground.java
+++ GUI/JPanelWithBackground.java
@@ -2,10 +2,16 @@ package GUI;
 
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,46 +19,51 @@ import javax.swing.JPanel;
 
 import Resourses.Map;
 /**
- * Code Taken from stackoverflow
- *
+ * JPanel with map as background
+ * @author
  *
  */
 public class JPanelWithBackground extends JPanel { 
-	Image imageOrg = null; 
-	Image image = null; 
+
+	BufferedImage image;
+	BufferedImage imageOrg;
 
 	{ 
 		addComponentListener(new ComponentAdapter() { 
 			public void componentResized(ComponentEvent e) { 
-				int w = JPanelWithBackground.this.getWidth(); 
-				int h = JPanelWithBackground.this.getHeight(); 
-				image = w>0&&h>0?imageOrg.getScaledInstance(w,h,java.awt.Image.SCALE_SMOOTH):imageOrg; 
+				
 				JPanelWithBackground.this.repaint(); 
 			} 
 		}); 
 	} 
-	public JPanelWithBackground(Image i) { 
-		imageOrg=i; 
-		image=i; 
+
+	public JPanelWithBackground(File i) { 
+		try {
+			imageOrg = ImageIO.read(i);
+			image = ImageIO.read(i);
+		}
+		catch (IOException e) {
+			System.err.println("IMAGEIO ERROR: JPANELWITHBACKGROUND");
+			e.printStackTrace();
+		}  
 		setOpaque(false); 
 	} 
 	public void paint(Graphics g) { 
+		int w = JPanelWithBackground.this.getWidth(); 
+		int h = JPanelWithBackground.this.getHeight(); 
+		if (w>0&&h>0) {
+			Graphics2D img = image.createGraphics();
+			Image reImage = imageOrg.getScaledInstance(w,h,java.awt.Image.SCALE_SMOOTH);
+			img.drawImage(reImage,0,0,JPanelWithBackground.this);
+			img.dispose();
+		}
+		else {
+			image = imageOrg;
+		}
 		if (image!=null) g.drawImage(image, 0, 0, null); 
 		super.paint(g); 
 	} 
-	
-//	public static void main(String args[]) {
-//
-//		ImageIcon g = new ImageIcon(ariel.getFile().getPath());
-//		Image image = g.getImage();
-//		JFrame f = new JFrame(""); 
-//		JPanel j = new JPanelWithBackground(image); 
-//		j.setLayout(new FlowLayout()); 
-//		j.add(new JButton("YoYo")); 
-//		j.add(new JButton("MaMa")); 
-//		f.add(j); 
-//		f.setVisible(true); 
-//	}
+
 } 
 
 
--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -1,55 +1,89 @@
 package GUI;
 
+
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import Resourses.Map;
-import java.awt.Color;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame{
 
-	private JPanel contentPane;
-	private Map ariel;
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MyFrame frame = new MyFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	private JPanel _panel;
+	private BufferedImage mapImage;
+	private int mapImageWidth, mapImageHeight;
+	private File mapFile;
+	private static Dimension d = new Dimension(400, 250);
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+
+	public MyFrame() {
+		initFrame();
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public MyFrame() {
-//		String path = "F:\\Works\\Ex2ObjectOriented\\src\\Resourses\\Ariel1.png";
-		ariel = new Map();
-		String path = ariel.getFile().getAbsolutePath();
-		System.out.println(path);
-		Image img = new ImageIcon(path).getImage();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanelWithBackground(img);
-		contentPane.setBackground(Color.BLACK);
-		contentPane.setBorder(new EmptyBorder(0,0,2000));
-		contentPane.setLayout(new FlowLayout());
-		setContentPane(contentPane);
+	public void initFrame() {
+		this.setPreferredSize(d);
+		try {
+			//ImageINITIALIZER
+			Map m = new Map();
+			mapFile = m.getFile();
+			mapImage = ImageIO.read(mapFile);
+			mapImageWidth = mapImage.getWidth();
+			mapImageHeight = mapImage.getHeight();
+		} 
+		catch (Exception e) {
+			System.err.println("ImageIO: Cant load image");
+		}
+		createMenu();
+		createPanel();
+		
+	}
+
+	public void createMenu() {
+		menuBar = new JMenuBar();
+		fileMenu = new JMenu("FILE");
+		fileMenu.setMnemonic(KeyEvent.VK_A);
+
+		menuBar.add(fileMenu);
+		this.setJMenuBar(menuBar);
+		System.out.println("add fileMenu and menu bar");
+
+
+	}
+
+	public void createPanel() {
+		_panel = new JPanel();
+		_panel.setLayout(new BorderLayout());
+
+		System.out.println("add panel");
+
+		this.add(_panel);
+	}
+	@Override
+	public void paint(Graphics g) {
+		int w = _panel.getWidth();
+		int h = _panel.getHeight();
+		Image img = mapImage.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+
+		g.drawImage(img, 0, 0, null);
+	}
+
+	public static void main(String[] args) {
+		MyFrame mainGame = new MyFrame();
+		mainGame.setSize(d);
+		mainGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainGame.setVisible(true);
 	}
 
 }
