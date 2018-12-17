4bbd662512d1b3002b739c986b09d8a2e5ad60e5
IgorKrol <igork08@gmail.com>
2018-12-17 15:48:15 +0200

    Gui works for now

--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -63,21 +63,25 @@ public class MyFrame extends JFrame{
 	}
 
 	public void createPanel() {
-		_panel = new JPanel();
+		_panel = new JPanelBG();
 		_panel.setLayout(new BorderLayout());
 
 		System.out.println("add panel");
 
 		this.add(_panel);
 	}
-	@Override
-	public void paint(Graphics g) {
-		int w = _panel.getWidth();
-		int h = _panel.getHeight();
-		Image img = mapImage.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+	public class JPanelBG extends JPanel{
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			int w = _panel.getWidth();
+			int h = _panel.getHeight();
+			Image img = mapImage.getScaledInstance(w, h, Image.SCALE_SMOOTH);
 
-		g.drawImage(img, 0, 0, null);
-	}
+			g.drawImage(img, 0, 0, null);
+		}
+	};
+
 
 	public static void main(String[] args) {
 		MyFrame mainGame = new MyFrame();
